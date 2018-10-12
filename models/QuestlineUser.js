5861139e653a74076cd2a4b1f02569064d6343c8
Steve Sefchick <stevesefchick@gmail.com>
2018-10-11 23:50:51 -0400

    Updates to QuestlineUserSchema

--- models/QuestlineUser.js
+++ models/QuestlineUser.js
@@ -3,9 +3,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const QuestlineUserSchema = new Schema({
-    userID: {type: Number},
     userName: {type: String},
-    dateCreated: {type:Date, default: Date.now}
+    password: {type: String},
+    dateCreated: {type:Date, default: Date.now},
+    lastUpdated: {type:Date, default: Date.now},
+    accountStatus: {type:String, enum:["Active","Inactive"] },
+    userInfo: {
+      firstName: {type: String},
+      lastName: {type:String},
+      emailAddress: {type:String}
+    }
+
   });
 
 module.exports = mongoose.model('questline_users', QuestlineUserSchema);
